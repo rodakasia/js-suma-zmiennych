bbdb6cbdc1c26b563eb1550093baf897690a834b
katarzyna <rodakasia@wp.pl>
2021-09-09 22:02:00 +0200

    Korekta formatowania

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 let firstNumber = 16;
 let secondNumber = 14;
 
-function suma (liczba1, liczba2) {let result = liczba1 + liczba2;
- return result;}
+function suma(liczba1, liczba2) {
+    let result = liczba1 + liczba2;
+    return result;
+}
 
-console.log (suma(46,47));
+console.log(suma(46,47) );
 
-console.log (suma (firstNumber, secondNumber));
+console.log(suma (firstNumber, secondNumber) );
 
-let dodaj = suma (100, 404);
+let dodaj = suma(100, 404);
 console.log(dodaj);
